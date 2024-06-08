7a5211ae2816ac3d3ec3ae0955bacda560255b3b
justprettyguyd12 <danshalutin@gmail.com>
2024-06-08 20:42:52 +0400

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
